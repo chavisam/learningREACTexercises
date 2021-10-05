1898ad9e3c65d473943d1d933ec58471618d52cd
Javier <jmanteca@jmanvel.com>
2021-10-05 17:00:48 +0000

    finished

--- exercises/01.5-building-html-from-arrays/app.jsx
+++ exercises/01.5-building-html-from-arrays/app.jsx
@@ -2,8 +2,16 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom"; //we use ReactDOM to render into the DOM
 
 // only update the value of this array
-const navlinkItems = [];
+const navlinkItems = ["Link to google.com", "Link to facebook.com", "Link to amazon.com"];
 
-const content = <ul className="nav">{navlinkItems}</ul>;
+const newLI = navlinkItems.map((LI) => (
+	<li className="nav-item">
+		<a className="nav-link" href="#">
+			{LI}
+		</a>
+	</li>
+));
+
+const content = <ul className="nav">{newLI}</ul>;
 
 ReactDOM.render(content, document.querySelector("#myDiv"));
--- exercises/03.2-a-real-component/app.jsx
+++ exercises/03.2-a-real-component/app.jsx
@@ -3,5 +3,27 @@ import ReactDOM from "react-dom";
 
 //create your function here
 
-//remember to use ReactDOM.render to include the component into the website
+const BootstrapCard = () => {
+	return (
+		<div className="card m-5">
+			<img
+				alt="Card image cap"
+				className="card-img-top"
+				src="https://github.com/4GeeksAcademy/react-tutorial-exercises/blob/master/.learn/assets/Dylan.png?raw=true"
+			/>
+			<div className="card-body">
+				<h5 className="card-title">Bob Dylan</h5>
+				<p className="card-text">
+					Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer/songwriter, author, and artist who has been an
+					influential figure in popular music and culture for more than five decades.
+				</p>
+				<a className="btn btn-primary" href="https://en.wikipedia.org/wiki/Bob_Dylan">
+					Go to wikipedia
+				</a>
+			</div>
+		</div>
+	);
+};
 
+//remember to use ReactDOM.render to include the component into the website
+ReactDOM.render(<BootstrapCard />, document.querySelector("#myDiv"));
